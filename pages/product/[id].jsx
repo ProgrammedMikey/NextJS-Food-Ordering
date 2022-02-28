656486091ddf4ad65c6c643a6ca3a002dc08e72e
Michael DaSilva <programmedmikey@gmail.com>
2022-02-28 02:33:47 +0000

    cart API added

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -2,10 +2,15 @@ import styles from "../../styles/Product.module.css"
 import Image from "next/image";
 import { useState } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux"
+import { addProduct } from "../../redux/cartSlice"
 
 const Product = ({pizza}) => {
     const [price, setPrice] = useState(pizza.prices[0]);
     const [size, setSize] = useState(0);
+    const [quantity, setQuantity] = useState(1);
+    const [extras, setExtras] = useState([]);
+    const dispatch = useDispatch();
 
     const changePrice = (number) => {
         setPrice(price + number);
@@ -22,10 +27,17 @@ const Product = ({pizza}) => {
 
         if(checked){
             changePrice(option.price)
+            setExtras((prev) => [...prev, option])
         }else {
             changePrice(-option.price)
+            setExtras(extras.filter((extra) => extra._id !== option._id));
         }
-    }
+    };
+
+    const handleClick = () => {
+        dispatch(addProduct({...pizza, extras, price, quantity}))
+    };
+    
     return (
         <div className={styles.container}>
             <div className={styles.left}>
@@ -68,8 +80,13 @@ const Product = ({pizza}) => {
                     ))}
                 </div>
                 <div className={styles.add}>
-                    <input type="number" defaultValue={1} className={styles.quantity} />
-                    <button className={styles.button}>Add to Cart</button>
+                    <input 
+                        onChange={(e) => setQuantity(e.target.value)}
+                        type="number"
+                        defaultValue={1} 
+                        className={styles.quantity} 
+                     />
+                    <button className={styles.button} onClick={handleClick()}>Add to Cart</button>
                 </div>
             </div>
         </div>
